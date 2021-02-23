639082d59540b31c481e551fc12c122c3c5481fd
Maxime Aubanel <maximeauba@gmail.com>
2021-02-23 04:15:43 +0100

    Ts everywhere + typechain + waffle + ethers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,10 @@
 import { HardhatUserConfig, task } from 'hardhat/config';
 
 import '@nomiclabs/hardhat-web3';
-import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+
 import 'hardhat-typechain';
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (_, { ethers }) => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: {
